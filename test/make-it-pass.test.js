88a215f4675e59bde524cea216d2acc379de5553
moranzargari <moran4151@gmail.com>
2018-05-13 17:56:52 +0300

    GREEN: add mult function to make-it-pass.test and test it --PASS

--- test/make-it-pass.test.js
+++ test/make-it-pass.test.js
@@ -1,10 +1,19 @@
 var test = require('tape'); // assign the tape library to the variable "test"
 
 function sum (a, b) {
-  a+b;
+  return a+b;
+}
+
+function mult (a, b) {
+  return a*b;
 }
 
 test('sum should return the addition of two numbers', function (t) {
   t.equal(3, sum(1, 2)); // make this test pass by completing the add function!
+  t.end();
+});
+
+test('mult should return the addition of two numbers', function (t) {
+  t.equal(2, mult(1, 2)); // make this test pass by completing the mult function!
   t.end();
 });
